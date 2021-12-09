c213e48b9a9a5d0f5efc4d85642e8af5706b03ea
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-12-10 00:37:14 +0800

    stale_deps broke, manually add wanted check

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,9 @@
 using Nanopass
 using Aqua
-Aqua.test_all(Nanopass)
+
+Aqua.test_ambiguities(Nanopass)
+Aqua.test_unbound_args(Nanopass)
+Aqua.test_undefined_exports(Nanopass)
+Aqua.test_project_extras(Nanopass)
+Aqua.test_deps_compat(Nanopass)
+Aqua.test_project_toml_formatting(Nanopass)
