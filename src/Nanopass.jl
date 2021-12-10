cceb7ebbb396ea31faa5e4da92d43f21a5867f6d
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-12-10 17:30:35 +0800

    `@language` works!

--- src/Nanopass.jl
+++ src/Nanopass.jl
@@ -1,29 +1,50 @@
 module Nanopass
 
+using Pipe
+using MacroTools
+
 macro language(language_name::Symbol, block::Expr)
-  for v in block.args
-    @show v
-  end
+  block = rmlines(block)
+  ss =
+    @pipe block.args |>
+          map(expr -> begin
+              entryName = expr.args[1]
+              sss = map(expr -> begin
+                  @assert @capture(expr, T_Symbol(fields__))
+                  :(struct $(T) <: $(entryName)
+                    $(fields...)
+                  end)
+                end, expr.args[2:end])
+              pushfirst!(sss, :(abstract type $(entryName) end))
+            end, _) |>
+          Iterators.flatten
   quote
-    @eval module $language_name end
+    @eval module $language_name
+    $(ss...)
+    end
   end
 end
 
 @language L0 begin
-  Expr
-  x::Symbol
-  c::Expr
-  Begin(es::[Expr], e::Expr)
-  If(test::Expr, body::Expr)
-  If(test::Expr, body::Expr, body2::Expr)
-  Lambda(vs::[Symbol], body::[Expr])
-  App(e::Expr, es::[Expr])
-
-  Stmt
-  Define(v::Symbol, e::Expr)
-
-  Prog
-  P(s::[Stmt])
+  Exp[
+    Var(x::Symbol)
+    Begin(es::Array{Expr}, e::Expr)
+    If(test::Expr, body::Expr, body2::Expr)
+    Lambda(vs::Array{Symbol}, body::Array{Expr})
+    App(e::Expr, es::Array{Expr})
+  ]
+  Stmt[
+    Define(v::Symbol, e::Expr)
+  ]
+  Prog[
+    P(s::Array{Stmt})
+  ]
 end
+
+@show L0
+@show L0.Exp
+@show L0.Stmt
+@show L0.Prog
+@show L0.Var(:a)
 
 end # module
