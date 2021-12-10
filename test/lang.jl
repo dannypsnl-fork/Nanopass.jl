179ce982f8aa4b4ec9625777b7563b803209d062
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-12-10 18:46:26 +0800

    lambda calculus application rule

--- test/lang.jl
+++ test/lang.jl
@@ -2,20 +2,27 @@ using Nanopass
 using Test
 
 @language L0 begin
-    Expr[
+    Exp[
         Var(x::Symbol)
-        Begin(es::Array{Expr}, e::Expr)
-        If(test::Expr, body::Expr, body2::Expr)
-        Lambda(vs::Array{Symbol}, body::Array{Expr})
-        App(e::Expr, es::Array{Expr})
-    ]
-    Stmt[
-        Define(v::Symbol, e::Expr)
-    ]
-    Prog[
-        P(s::Array{Stmt})
+        Lambda(vs::Array{Var}, body::Exp)
+        App(e::Exp, es::Array{Exp})
     ]
 end
 
 using .L0
-@test typeof(Lambda([:a], [Var(:a)])) == Lambda
+using MLStyle
+
+function app(e::Exp, args::Exp...)
+    @match e begin
+        Lambda(vs, body) && if length(vs) == length(args)
+        end => begin
+            d = Dict()
+            for (v, a) in zip(vs, args)
+                d[v] = a
+            end
+            haskey(d, body) ? d[body] : body
+        end
+    end
+end
+
+@test app(Lambda([Var(:a)], Var(:a)), Var(:b)) == Var(:b)
