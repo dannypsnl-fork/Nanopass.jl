ddb505086262af6f965f6e5df4ffdf06faad515a
dannypsnl <dannypsnl@users.noreply.github.com>
2021-12-10 12:23:29 +0000

    deploy: 8842681338ae9d6abbe140f92a672676bfed5067

---
+++ search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Nanopass.jl","page":"Nanopass.jl","title":"Nanopass.jl","text":"","category":"section"},{"location":"","page":"Nanopass.jl","title":"Nanopass.jl","text":"Documentation for Nanopass.jl","category":"page"}]
+}
