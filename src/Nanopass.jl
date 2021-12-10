e9462810cae4ce84977065ea4bb2045bab3f4589
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-12-10 14:02:51 +0800

    try to build language block

--- src/Nanopass.jl
+++ src/Nanopass.jl
@@ -1,5 +1,24 @@
 module Nanopass
 
-greet() = print("Hello World!")
+macro language(language_name::Symbol, block)
+    quote
+        @eval module $language_name
+            $block
+        end
+    end
+end
+
+@language L0 begin
+  Expr
+    x::Symbol
+    c::Expr
+    Begin(es::[Expr], e::Expr)
+    If(test::Expr, body::Expr)
+    If(test::Expr, body::Expr, body2::Expr)
+    Lambda(vs::[Symbol], body::[Expr])
+    App(e::Expr, es::[Expr])
+  Stmt
+    Define(v::Symbol, e::Expr)
+end
 
 end # module
