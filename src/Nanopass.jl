69d4a6976cef3c96f8a1c9617936045a486f5ab3
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-12-10 14:29:19 +0800

    show internal

--- src/Nanopass.jl
+++ src/Nanopass.jl
@@ -1,24 +1,29 @@
 module Nanopass
 
-macro language(language_name::Symbol, block)
-    quote
-        @eval module $language_name
-            $block
-        end
-    end
+macro language(language_name::Symbol, block::Expr)
+  for v in block.args
+    @show v
+  end
+  quote
+    @eval module $language_name end
+  end
 end
 
 @language L0 begin
   Expr
-    x::Symbol
-    c::Expr
-    Begin(es::[Expr], e::Expr)
-    If(test::Expr, body::Expr)
-    If(test::Expr, body::Expr, body2::Expr)
-    Lambda(vs::[Symbol], body::[Expr])
-    App(e::Expr, es::[Expr])
+  x::Symbol
+  c::Expr
+  Begin(es::[Expr], e::Expr)
+  If(test::Expr, body::Expr)
+  If(test::Expr, body::Expr, body2::Expr)
+  Lambda(vs::[Symbol], body::[Expr])
+  App(e::Expr, es::[Expr])
+
   Stmt
-    Define(v::Symbol, e::Expr)
+  Define(v::Symbol, e::Expr)
+
+  Prog
+  P(s::[Stmt])
 end
 
 end # module
