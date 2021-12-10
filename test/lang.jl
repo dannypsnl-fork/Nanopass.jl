3fa19aa3432f6ba1ac32fefcace76a6bb19b2d47
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-12-10 19:22:08 +0800

    eval

--- test/lang.jl
+++ test/lang.jl
@@ -5,12 +5,20 @@ using Test
     Exp[
         Var(x::Symbol)
         Lambda(vs::Array{Var}, body::Exp)
-        App(e::Exp, es::Array{Exp})
+        App(f::Exp, args::Array{Exp})
     ]
 end
 
 using .L0
 using MLStyle
+
+function eval(e::Exp)
+    @match e begin
+        Var(x) => Var(x)
+        Lambda(vs, body) => Lambda(vs, eval(body))
+        App(f, args) => eval(app(f, args...))
+    end
+end
 
 function app(e::Exp, args::Exp...)
     @match e begin
@@ -25,4 +33,5 @@ function app(e::Exp, args::Exp...)
     end
 end
 
-@test app(Lambda([Var(:a)], Var(:a)), Var(:b)) == Var(:b)
+@test eval(Var(:a)) == Var(:a)
+@test eval(App(Lambda([Var(:a)], Var(:a)), [Var(:b)])) == Var(:b)
